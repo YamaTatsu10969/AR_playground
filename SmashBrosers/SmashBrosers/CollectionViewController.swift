1b8481a67491cd8ab6a60f6a13c20caf2cad1cf0
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-01-16 22:09:51 +0900

    feat:add image

--- SmashBrosers/SmashBrosers/CollectionViewController.swift
+++ SmashBrosers/SmashBrosers/CollectionViewController.swift
@@ -12,11 +12,18 @@ class CollectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mario()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var intoImage: new!
+    
+    func mario(){
+        intoImage.image = UIImage(named: "mario_result")
+    }
+    
     @IBAction func backCollectButton(_ sender: Any) {
         self.performSegue(withIdentifier: "collectSegue", sender: nil)
     }
